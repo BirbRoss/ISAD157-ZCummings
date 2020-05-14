7f89f93f0dd618d461399e39d364ab025f2609e1
BirbRoss <55706678+BirbRoss@users.noreply.github.com>
2020-05-14 09:26:55 +0100

    Initial commit

---
+++ ZCummings_SQL_Interface/ZCummings_SQL_Interface/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ZCummings_SQL_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableSwitch = new System.Windows.Forms.ComboBox();
+            this.gridOutput = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.gridOutput)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableSwitch
+            // 
+            this.tableSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.tableSwitch.FormattingEnabled = true;
+            this.tableSwitch.Items.AddRange(new object[] {
+            "Users Table",
+            "Friendships Table",
+            "Messages Table",
+            "Universities Table",
+            "Workplace Table"});
+            this.tableSwitch.Location = new System.Drawing.Point(6, 325);
+            this.tableSwitch.MaxDropDownItems = 5;
+            this.tableSwitch.MaxLength = 5;
+            this.tableSwitch.Name = "tableSwitch";
+            this.tableSwitch.Size = new System.Drawing.Size(606, 32);
+            this.tableSwitch.TabIndex = 0;
+            this.tableSwitch.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // gridOutput
+            // 
+            this.gridOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridOutput.Location = new System.Drawing.Point(6, 19);
+            this.gridOutput.Name = "gridOutput";
+            this.gridOutput.Size = new System.Drawing.Size(606, 300);
+            this.gridOutput.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gridOutput);
+            this.groupBox1.Controls.Add(this.tableSwitch);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(620, 369);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Database view and Selection";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 393);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridOutput)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox tableSwitch;
+        private System.Windows.Forms.DataGridView gridOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ZCummings_SQL_Interface/ZCummings_SQL_Interface/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ZCummings_SQL_Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This was a mistake but cannot be removed without breaking everything.
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        //should be called tableSwitch
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Users
+            if (tableSwitch.SelectedIndex == 0)
+            {
+
+                //Generates Login details from DBConnect Class
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+
+
+                using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+                {
+                    //Preps query and opens connection (May not work due to connection issues)
+                    string query = "SELECT * FROM isad157_zcummings.users";
+                    connection.Open();
+
+                    //Gives query and returns with results
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable UserDataTable = new DataTable();
+                    sqlDA.Fill(UserDataTable);
+
+                    //Clears grid and outputs results to grid
+                    gridOutput.ClearSelection();
+                    gridOutput.DataSource = UserDataTable;
+                }
+            }
+            //Friendships
+            if (tableSwitch.SelectedIndex == 1)
+            {
+                //Generates Login details from DBConnect Class
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+
+
+                using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+                {
+                    //Preps query and opens connection
+                    string query = "SELECT * FROM isad157_zcummings.friendships";
+                    connection.Open();
+
+                    //Gives query and returns with results
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable MessagesDataTable = new DataTable();
+                    sqlDA.Fill(MessagesDataTable);
+
+                    //Clears grid and outputs results to grid
+                    gridOutput.ClearSelection();
+                    gridOutput.DataSource = MessagesDataTable;
+                }
+            }
+            //Messages
+            if (tableSwitch.SelectedIndex == 2)
+            {
+                //Generates Login details from DBConnect Class
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+
+
+                using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+                {
+                    //Preps query and opens connection
+                    string query = "SELECT * FROM isad157_zcummings.messages";
+                    connection.Open();
+
+                    //Gives query and returns with results
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable MessageDataTable = new DataTable();
+                    sqlDA.Fill(MessageDataTable);
+
+                    //Clears grid and outputs results to grid
+                    gridOutput.ClearSelection();
+                    gridOutput.DataSource = MessageDataTable;
+                }
+            }
+            //Universities
+            if (tableSwitch.SelectedIndex == 3)
+            {
+                //Generates Login details from DBConnect Class
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+
+
+                using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+                {
+                    //Preps query and opens connection
+                    string query = "SELECT * FROM isad157_zcummings.universities";
+                    connection.Open();
+
+                    //Gives query and returns with results
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable UniDataTable = new DataTable();
+                    sqlDA.Fill(UniDataTable);
+
+                    //Clears grid and outputs results to grid
+                    gridOutput.ClearSelection();
+                    gridOutput.DataSource = UniDataTable;
+                }
+            }
+            //Workplace
+            else
+            {
+                //Generates Login details from DBConnect Class
+                string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+
+
+                using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+                {
+                    //Preps query and opens connection
+                    string query = "SELECT * FROM isad157_zcummings.workplace";
+                    connection.Open();
+
+                    //Gives query and returns with results
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                    DataTable WorkDataTable = new DataTable();
+                    sqlDA.Fill(WorkDataTable);
+
+                    //Clears grid and outputs results to grid
+                    gridOutput.ClearSelection();
+                    gridOutput.DataSource = WorkDataTable;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Tried to make the combo box read only, didn't work and now can't delete or everything breaks
+            tableSwitch.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+    }
+}
